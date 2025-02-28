c4058d12159a7afa3c3f7e9e443ee3e479ded0e9
one-donovan <odonovanone@gmail.com>
2025-03-01 02:14:29 +0400

    final
    * Добавляет наставника/разметку
    
    * добавляет разметку
    
    * добавляет разметку
    
    * добавляет разметку
    
    * Добавляет стили и изображения
    
    * update style 1.1
    
    * update style 1.2
    
    * update style 1.3
    
    * update style 1.4
    
    * update style 1.5
    
    * update style 1.6
    
    * update style 1.7
    
    * update style 1.8
    
    * update style 1.9
    
    * update style 2.0  адаптив шапки
    
    * update 2.0 reserve
    
    * update style 2.1
    
    * update style 2.2
    
    * update 2.3 style
    
    * update 2.4 добавляет векторную графику
    
    * update 2.5 style
    
    * update style 2.6
    
    * update style 2.7
    
    * update style 2.8
    
    * update style 2.9
    
    * update style 3.0
    
    * update style 3.1
    
    * update style 3.2
    
    * update style 3.3
    
    * update style 3.4
    
    * update style 3.4
    
    * update style 3.5
    
    * update style 3.6
    
    * update style 3.7
    
    * update style 3.8

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,11 @@
-/* в этот файл добавляет скрипты*/
+const menuButton = document.querySelector('.nav__menu-button');
+const menuList = document.querySelector('.nav__list-inner');
+const map = document.querySelector('.map__frame');
+
+map.classList.remove('map__frame--nojs');
+menuList.classList.remove('nav__list-inner--nojs');
+
+menuButton.addEventListener('click', () => {
+  menuList.classList.toggle('nav__list-inner--opened');
+  menuButton.classList.toggle('nav__menu-button--opened');
+});
