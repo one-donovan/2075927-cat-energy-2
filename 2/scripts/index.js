1f9061fd1fef698f30edeaa586a4d74e457fefc8
one-donovan <odonovanone@gmail.com>
2025-02-28 22:14:50 +0000

    Deploying to gh-pages from @ one-donovan/2075927-cat-energy-2@c4058d12159a7afa3c3f7e9e443ee3e479ded0e9 🚀

--- 2/scripts/index.js
+++
@@ -1,1 +0,0 @@
-var e=document.querySelector(".nav__menu-button"),n=document.querySelector(".nav__list-inner"),t=document.querySelector(".map__frame");t.classList.remove("map__frame--nojs");n.classList.remove("nav__list-inner--nojs");e.addEventListener("click",()=>{n.classList.toggle("nav__list-inner--opened"),e.classList.toggle("nav__menu-button--opened")});
